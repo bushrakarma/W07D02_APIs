c6d859ad5ac1ebd1641e89f24abd00c422e1e8f2
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-05-10 13:49:50 +0300

    update

--- main.js
+++ main.js
@@ -13,26 +13,45 @@ const users = [
   { name: "Mark", age: 19 },
 ];
 
-app.get("/users", (request, response)=>{
-    response.status(200);
-    response.json(users)
-})
-
-app.get("/user/:name", (req, res) => {
-    const user = req.params.name
-    const found = users.find((element) => {
-        return element.name === user;
-      });
-
-      if (found) {
-        res.status(200);
-        res.json(found);
-      } else {
-        res.status(404);
-        res.json("User not found");
-      }
+app.get("/users", (request, response) => {
+  response.status(200);
+  response.json(users);
 });
 
+app.get("/user/:name", (req, res) => {
+  const user = req.params.name;
+  const found = users.find((element) => {
+    return element.name === user;
+  });
+  if (found) {
+    res.status(200);
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("User not found");
+  }
+});
+
+app.post("/create/user", (req, res) => {
+  const newUser = {
+    name: req.body.name,
+    age: req.body.age,
+  };
+
+  users.push(newUser);
+  res.status(201);
+  res.json(users);
+});
+
+app.get('/first-user', (req, res) =>{
+    const first = users[0];
+    res.status(200);
+    res.json(first)
+})
+
+app.get('/', (req, res) =>{
+    res.send("Hello World!")
+})
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
